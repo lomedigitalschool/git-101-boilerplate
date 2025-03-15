83a8097be5233a7791cfd9cbf9cb8e950a56f239
Romuald <147240143+Moruald001@users.noreply.github.com>
2025-03-15 12:13:43 +0000

    Update welcome-message-with-date.sh
    replace the comment in line 7 with "Save the date in the format $DAY_NAME, $DAY $MONTH $YEAR"

--- welcome-message-with-date.sh
+++ welcome-message-with-date.sh
@@ -4,7 +4,7 @@
 echo "what's your full name"
 read USER_FULLNAME
 
-#I save the current date in TODAY by adding the suffix corresponding to the day
+#Save the date in the format "$DAY_NAME, $DAY $MONTH $YEAR"
 TODAY=$(date +"%A, %B %d$(date +%d | awk '{print ($1==1||$1==21||$1==31)?"st":($1==2||$1==22)?"nd":($1==3||$1==23)?"rd":"th"}') %Y")
 
 #I display the message with user full name and the current date
