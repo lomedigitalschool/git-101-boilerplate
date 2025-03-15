2d91e3b2841af249a32e87aa4f8e82a15b64bde2
yannick Namboure <yanoboston@gmail.com>
2025-03-15 20:43:01 +0000

    Backup the code folder

--- code-backups.sh
+++ code-backups.sh
@@ -5,14 +5,14 @@ CODE_ZIP="$HOME/code"
 BACKUP_FILE="$HOME/backup-code.zip"
 
 # check if backrest exist
-if [ ! -d "$CODE_DIR" ]; then
-    echo "Erreur : Le dossier $CODE_DIR n'existe pas."
+if [ ! -d "$CODE_ZIP" ]; then
+    echo "Erreur : Le dossier $CODE_ZIP n'existe pas."
     exit 1
 fi
 
 # Create a zip archive of the code folder
-echo "Compression du dossier $CODE_DIR en $BACKUP_FILE..."
-zip -r "$BACKUP_FILE" "$CODE_DIR"
+echo "Compression du dossier $CODE_ZIP en $BACKUP_FILE..."
+zip -r "$BACKUP_FILE" "$CODE_ZIP"
 
 # check if compression exists
 if [ $? -eq 0 ]; then
